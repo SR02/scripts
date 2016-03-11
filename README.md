4d1c6f8e325aeb090709bbd60355f97ea64cedaa
Brian Salvaggio <brian.salvaggio@dycominc.com>
2016-03-10 20:28:06 -0500

    Adding first script and README.md

---
+++ README.md
@@ -0,0 +1,1 @@
+Dump of scripts I'm willing/allowed to share
---
+++ block-access.ps1
@@ -0,0 +1,14 @@
+﻿#Parameters for the script
+param (
+    [string]$username = $(throw "-username is required.")
+)
+#Setup Logging
+if ([System.Diagnostics.EventLog]::SourceExists("Block-Access") -eq $false){
+    New-Eventlog -LogName Application -Source "Block-Access"
+}
+$shares = Get-SmbShare |where {$_.name.endswith("$") -ne $_}
+#Loop for each share on the server and remove the users access
+Write-EventLog -LogName Application -Source "Block-Access" -entryType Information -EventID 1337 -Message "User infected!!! Removing Acess for User: $username from all shares"
+foreach ($share in $shares) {
+Block-SmbShareAccess -name $share.name -AccountName $username -force
+}
